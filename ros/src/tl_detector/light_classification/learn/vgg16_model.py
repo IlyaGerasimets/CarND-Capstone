e874a5ed08177bccf7fc61a3d525a895530abfde
Alexei Strots <strotz@gmail.com>
2017-10-29 08:26:22 -0700

    traint vgg on scaled down images

--- ros/src/tl_detector/light_classification/learn/vgg16_model.py
+++ ros/src/tl_detector/light_classification/learn/vgg16_model.py
@@ -7,10 +7,14 @@ from keras.models import Model
 import numpy as np
 
 nb_classes = 3
+input_shape = (800, 600, 3)
+source_shape = (160, 120, 3) # original VGG expects input 224x224x3
+batch_size = 64
+nb_epoch = 10
 
 def get_model():
-    # load model without last layer (expected input 224x224x3)
-    base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    # load model without last layer
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=source_shape)
 
     # new model with 3 classes
     x = base_model.output
@@ -22,19 +26,34 @@ def get_model():
     for layer in base_model.layers:
         layer.trainable = False
 
-
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-
     return model
+
+def train_model(model):
+    datagen = image.ImageDataGenerator(width_shift_range=.2, height_shift_range=.2, shear_range=0.05, zoom_range=.1, 
+        fill_mode='nearest', rescale=1. / 255)
+
+    image_data_gen = datagen.flow_from_directory('images', target_size=(source_shape[0], source_shape[1]), 
+        classes=['green', 'yellow', 'red'], batch_size=batch_size)
+
+    model.fit_generator(image_data_gen, steps_per_epoch=50)
+
+
+def save_model_state(model):
+    filename = 'nets/light_classifier_model_vgg16_%sx%s.h5'% (source_shape[0], source_shape[1])
+    model.save(filename)
 
 
 model = get_model()
+model.summary()
+train_model(model)
+save_model_state(model)
 
-img_path = 'images/img_0_0.png'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
+# img_path = 'images/img_0_0.png'
+# img = image.load_img(img_path, target_size=(224, 224))
+# x = image.img_to_array(img)
+# x = np.expand_dims(x, axis=0)
+# x = preprocess_input(x)
 
-features = model.predict(x)
-print(features)
+# features = model.predict(x)
+# print(features)
